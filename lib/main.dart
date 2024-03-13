3094c9e0f004e0c1cf69dd496c7b5e0f96ef4903
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2024-03-13 15:12:08 +0700

    Feat: added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,83 +4,118 @@ void main() {
   runApp(const MainApp());
 }
 
-TextEditingController usrController = TextEditingController();
-TextEditingController passwordController = TextEditingController();
-
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(body: MyCustomForm()),
     );
   }
 }
 
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  TextEditingController usrController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool _obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return ListView(
       children: [
-        const Text('Login'),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: usrController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Username',
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Image.asset('family.png')),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            controller: passwordController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Password',
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: Text(
+                'Masukkan username dan kata sandi anda!',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                controller: usrController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Username',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                obscureText: _obscurePassword,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                  // suffixIcon: IconButton(
+                  //   icon: Icon(_obscurePassword
+                  //       ? Icons.visibility
+                  //       : Icons.visibility_off),
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       _obscurePassword =
+                  //           !_obscurePassword; // Mengubah kebalikan status
+                  //     });
+                  //   },
+                  // ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    foregroundColor: Colors.white),
+                onPressed: () {
+                  String username = usrController.text;
+                  String password = passwordController.text;
+
+                  if (username.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('Username kosong'),
+                      duration: Duration(seconds: 2),
+                    ));
+                    return;
+                  }
+
+                  if (password.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('Kata sandi kosong'),
+                      duration: Duration(seconds: 2),
+                    ));
+                    return;
+                  }
+
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('username: $username, password: $password'),
+                    duration: Duration(seconds: 2),
+                  ));
+                },
+                child: const Text('Masuk'),
+              ),
+            )
+          ],
         ),
-        // ignore: sized_box_for_whitespace
-        Container(
-          padding: EdgeInsets.all(10),
-          width: double.infinity,
-          child: TextButton(
-            style: TextButton.styleFrom(
-                backgroundColor: Colors.amber, foregroundColor: Colors.white),
-            onPressed: () {
-              String username = usrController.text;
-              String password = passwordController.text;
-
-              if (username.isEmpty) {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text('Username kosong'),
-                  duration: Duration(seconds: 2),
-                ));
-                return;
-              }
-
-              if (password.isEmpty) {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text('Kata sandi kosong'),
-                  duration: Duration(seconds: 2),
-                ));
-                return;
-              }
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text('username: $username, password: $password'),
-                duration: Duration(seconds: 2),
-              ));
-            },
-            child: const Text('Masuk'),
-          ),
-        )
       ],
     );
   }
